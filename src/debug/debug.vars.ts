42d02559649326a5f9f2fd6d6da0df01ee4cd3a6
Шелипов Дмитрий <dshelipov@cloudfactory.ru>
2025-04-14 20:37:32 +0300

    fix DebugVars

--- src/debug/debug.vars.ts
+++ src/debug/debug.vars.ts
@@ -1,9 +1,10 @@
 export const DebugVars = !__DEV__ ? undefined : {
   showScreenNames: false,
+  // изменение темы пока не работает
   enableToggleThemeButton: false,
 };
 
 if (DebugVars) {
-  DebugVars.showScreenNames = true;
+  // DebugVars.showScreenNames = true;
   // DebugVars.enableToggleThemeButton = true;
 }
