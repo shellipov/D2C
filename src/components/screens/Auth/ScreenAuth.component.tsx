d985e58cb2a67b3c4beacdec8a5e631750843b37
Шелипов Дмитрий <dshelipov@cloudfactory.ru>
2025-04-24 19:40:16 +0300

    fix auth & category screens

--- src/components/screens/Auth/ScreenAuth.component.tsx
+++ src/components/screens/Auth/ScreenAuth.component.tsx
@@ -1,5 +1,4 @@
-import { Keyboard, StyleSheet, TouchableWithoutFeedback } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 import React, { useCallback, useEffect, useMemo } from 'react';
 import { TextUI } from '../../ui/TextUI';
 import { TextInputUI } from '../../ui/TextInputUI';
@@ -41,22 +40,13 @@ export const ScreenAuth = observer((props: Props) => {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <Screen isError={userStore.isError} onRefresh={userStore.refresh}>
-        <Col style={[styles.block, { backgroundColor: theme.color.bgAdditionalTwo }]}>
+        <Col flex alignItems={'center'} justifyContent={'center'} bg={theme.color.bgAdditionalTwo}>
           <TextUI text={'Введите Ваш ник'} size={'title'} />
           <TextUI text={'и постарайтесь не забыть его'} size={'small'} />
-          <TextInputUI value={name} textSize={'large'} onChangeText={onChangeText} style={styles.input} />
+          <TextInputUI value={name} textSize={'large'} onChangeText={onChangeText} textAlign={'center'} />
           <ButtonUI title={'Войти'} onPress={onPressLogin} disabled={isButtonDisabled} />
         </Col>
       </Screen>
     </TouchableWithoutFeedback>
   );
-});
-
-const styles = StyleSheet.create({
-  block: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: { textAlign: 'center' },
 });
